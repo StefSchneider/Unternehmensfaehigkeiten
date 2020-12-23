8d7eeabf5e37fd1d61de5dc9f7afb2ef548ced3b
schne <schneider.solingen@web.de>
2020-12-23 17:41:43 +0100

    Refactoring und Dokumentation

--- Persistenz_UF.py
+++ Persistenz_UF.py
@@ -43,46 +43,48 @@ class Speicherinhalt:
 
 class Persistenz:
 
-    def __init__(self, wurzel: str, datenspeicher = False, speicherart: str = "Hauptspeicher"):
-        self.__persistenz_datenspeicher = datenspeicher  # steuert, ob die Daten im Datenspeicher abgelegt werden
+    def __init__(self, wurzel: str, datenspeicher: bool = False, speicherart: str = "Hauptspeicher"):
+        self.__wurzel: str = wurzel
+        self.__persistenz_datenspeicher: bool = datenspeicher  # steuert, ob die Daten im Datenspeicher abgelegt werden
         self.datenspeicher: dict = {wurzel: {}} # legt einen neuen leeren Datenspeicher für den Microservice an
+#        self.datenspeicher = Baum(wurzel)
         self.speicherart: str = speicherart
 
     def zerlege_pfad(self, pfad: str) -> list:
-        __pfad: str = pfad
+        __pfad_ein: str = pfad
         __hierarchien: list = [""]
-        if __pfad[-1] == "/":
-            __pfad = __pfad.rstrip("/")
+        if __pfad_ein[-1] == "/":
+            __pfad = __pfad_ein.rstrip("/")
             # Um ein leeres Listenelement zu vermeiden, wird ein mögliches "/" auf der letzten Position gelöscht
-        __hierarchien.extend(__pfad.split("/"))
+        __hierarchien.extend(__pfad_ein.split("/"))
         for __elemente in __hierarchien:
             if __elemente == "":
                 __hierarchien.remove("")
 
         return __hierarchien
 
-    def erzeuge_schluessel_neueintrag(self, nummer_ein: int) -> str:
+    def erzeuge_schluessel_neueintrag(self, nummer: int) -> str:
         """
 
-        :param nummer_ein:
+        :param nummer:
         :return:
         """
-        __nummer_ein = nummer_ein
+        __nummer_ein: int = nummer
         __schluessel: str = ""
         __schluessel = str(__nummer_ein) + "_" + str((random.randint(1, 1000) * random.randint(1, 1000)))
 
         return __schluessel
 
-    def ermittele_letzten_eintrag_hierarchie(self, hierarchie_ein: list) -> int:
-        __hierarchie_ein = hierarchie_ein
+    def ermittele_letzten_eintrag_hierarchie(self, hierarchien: list) -> int:
+        __hierarchien_ein: list = hierarchien
         __ressourcen: list = [0]
-        for __eintrag in __hierarchie_ein:
+        for __eintrag in __hierarchien_ein:
             if not __eintrag.startswith("h_"):  # schließt Hierachien bei der Betrachtung aus
                 __ressourcen.append(int(__eintrag))  # erfasse vorhandene Ressourcen in Hierachie
 
         return max(__ressourcen)
 
-    def zeige_datenspeicher_json(self, hierarchie_ein: dict):
+    def zeige_datenspeicher_json(self, hierarchie: dict):
         """
         Da der Inhalt eines Dictionaries in einer Zeile angezeigt wird, sind die einzelnen Hierarchien schwer zu
         erkennen. Mit der Methode erfolgt die Anzeige in Form eines JSON-Formats. Zur besseren Übersichtlichkeit
@@ -90,10 +92,11 @@ class Persistenz:
         :param hierarchie_ein:
         :return: None
         """
-        __datenspeicher_json = json.dumps(hierarchie_ein, sort_keys = True, indent = 4)
+        __hierarchie_ein: dict = hierarchie
+        __datenspeicher_json = json.dumps(__hierarchie_ein, sort_keys = True, indent = 4)
         print(__datenspeicher_json)
 
-    def ersetze_letzen_schluessel(self, ebenen: list) -> list:
+    def ersetze_letzen_schluessel(self, hierarchien: list) -> list:
         """
         Hierarchien sind in dem Dictionary mit dem Prefix "h_" gekennzeichnet. Der tatsächliche Speicher innerhalb der
         Hierarchie enthält kein solches Prexif. Um auf den Wert des angeforderten Speichers zugreifen zu können, muss
@@ -101,12 +104,50 @@ class Persistenz:
         :param ebenen: Liste der Hierarchien
         :return: Liste der Hierarchien mit dem geänderten letzten Schlüsselwort
         """
-        __ebenen = ebenen
-        __letztes_schluesselwort = __ebenen[-1]
+        __hierarchien_ein = hierarchien
+        __hierarchien: list = []
+        __hierarchien = __hierarchien_ein.copy() # legt eine Kopie an, um Originalliste nicht zu verändern
+        __letztes_schluesselwort = __hierarchien_ein[-1]
         __letztes_schluesselwort = __letztes_schluesselwort.rsplit("/", 1)[1]
-        __ebenen[-1] = __letztes_schluesselwort
+        __hierarchien[-1] = __letztes_schluesselwort
 
-        return __ebenen
+        return __hierarchien
+
+
+    def erzeuge_speicherinhalt(self, hierarchien: list, inhalt: dict):  # neue Methode mit Baumstruktur
+        """
+        Mit der Methode wird ein leeres Speicherelement unter der richtigen Ressource angelegt, das später über die
+        Methode aendere_speicherinhalt gefüllt wird.
+        Grundlage: CRUD - Create
+        :return: neu angelegtes Speicherelement als leeres Dictionary
+        """
+        __hierarchen_ein: list = hierarchien
+        __inhalt_ein: dict = inhalt
+        __neues_speicherelement = Speicherinhalt()
+        __rueckgaben_daten_aus: dict = {"daten": None,
+                                        "rueckmeldung": "",
+                                        "erzeuge_ressource_erfolgreich": False,
+                                        "fehlercode": 0}
+        __ressource_vorhanden: bool = True
+        __aktuelle_ebene = self.datenspeicher
+        for __position, __schluessel in enumerate(__hierarchen_ein):
+            if __schluessel in __aktuelle_ebene.keys():
+                __aktuelle_ebene = __aktuelle_ebene[__schluessel]
+            else:
+                __rueckgaben_daten_aus["rueckmeldung"] = "Ressource kann nicht angelegt werden:" \
+                                                         + " ... " \
+                                                         + "/".join(__hierarchen_ein[__position - 1:]) \
+                                                         + " fehlt."
+                __ressource_vorhanden = False
+                break
+        if __ressource_vorhanden:
+            __aktuelle_ebene.update(__inhalt_ein)
+            __rueckgaben_daten_aus["daten"] = __neues_speicherelement.speicherobjekt
+            print(type(__neues_speicherelement.speicherobjekt))
+            __rueckgaben_daten_aus["erzeuge_ressource_erfolgreich"] = True
+            __rueckgaben_daten_aus["fehlercode"] = 201
+
+        return json.dumps(__rueckgaben_daten_aus)
 
 
     def erzeuge_speicherinhalt(self, ebenen: list, inhalt: dict):
