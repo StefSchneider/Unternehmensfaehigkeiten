526d0de6047f6b5bf3c07af664e4e3e396f1ac80
schne <schneider.solingen@web.de>
2020-12-26 17:14:26 +0100

    Refactoring und Dokumentation

---
+++ Test Dict.py
@@ -0,0 +1,40 @@
+test_datenspeicher = {
+    "prints": {
+        "1000_88536": {
+            "auftrag": "drucke_daten",
+            "auftraggeber": "C:/Users/schne/Coding/Python/Projects/Unternehmensfaehigkeiten/ms_hellos.py",
+            "id": "1000",
+            "server_port_auftraggeber": 31001,
+            "text": "test1"
+        },
+        "2000_146705": {
+            "3000_706482": {
+                "5000_332990": {
+                    "auftrag": "drucke_daten",
+                    "auftraggeber": "C:/Users/schne/Coding/Python/Projects/Unternehmensfaehigkeiten/ms_hellos.py",
+                    "id": "5000",
+                    "server_port_auftraggeber": 31001,
+                    "text": "test5"
+                },
+	"6000_552987": {
+                    "auftrag": "drucke_daten",
+                    "auftraggeber": "C:/Users/schne/Coding/Python/Projects/Unternehmensfaehigkeiten/ms_hellos.py",
+                    "id": "5000",
+                    "server_port_auftraggeber": 31001,
+                    "text": "test6"
+                },
+
+                "auftrag": "drucke_daten",
+                "auftraggeber": "C:/Users/schne/Coding/Python/Projects/Unternehmensfaehigkeiten/ms_hellos.py",
+                "id": "3000",
+                "server_port_auftraggeber": 31001,
+                "text": "test3"
+            },
+            "auftrag": "drucke_daten",
+            "auftraggeber": "C:/Users/schne/Coding/Python/Projects/Unternehmensfaehigkeiten/ms_hellos.py",
+            "id": "2000",
+            "server_port_auftraggeber": 31001,
+            "text": "test2"
+        }
+    }
+}
